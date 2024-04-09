35c531e6e9aac30235567812aa055f5f8eb1e3c2
NBdadasama <chen123asd@foxmail.com>
2024-04-09 22:28:39 +0800

    目前稳定版本v1

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -50,8 +50,7 @@ public class UserController {
 
         List<User> users = userService.getAllUser(pageNum, pageSize);
 
-        List<UserVo> userVos = users.stream().map((User user) ->
-        {
+        List<UserVo> userVos = users.stream().map((User user) -> {
             return new UserVo(user);
         }).collect(Collectors.toList());
         return Result.success(userVos);//返回用户list
@@ -60,7 +59,7 @@ public class UserController {
     @GetMapping("/get/{userId}")//获得单个用户
     public Result getOneUser(@PathVariable("userId") String userId) {
         User user = userService.getUser(userId);
-        if (user==null){
+        if (user == null) {
             return Result.failure(Result.ResultCode.USER_NOT_EXISTED);//用户不存在
         }
         return Result.success(new UserVo(user));//返回用户list
@@ -78,8 +77,7 @@ public class UserController {
         if (x > 0) {//删除用户
             functionService.saveRecord(BehavioralRecordBo.RecordCode.DELETE_USER.getMessage(), user.toString());
             return Result.success("删除成功");
-        } else
-            return Result.failure(Result.ResultCode.DELETE_FAILURE, "删除失败");
+        } else return Result.failure(Result.ResultCode.DELETE_FAILURE, "删除失败");
     }
 
 
@@ -102,28 +100,26 @@ public class UserController {
 
 
     @GetMapping("/get/verification/code")//获得验证码
-    public Result getVerificationCode(@RequestParam String userId,
-                                      @RequestParam int category) throws MessagingException {//获取验证码,后面是类别
+    public Result getVerificationCode(@RequestParam String userId, @RequestParam int category) throws MessagingException {//获取验证码,后面是类别
 
         String code = userService.generateVerifyCode(6);//默认六位验证码
 
         User user = userService.getUser(userId);
-        if (user == null)
-            return Result.failure(Result.ResultCode.USER_NOT_EXISTED);//用户不存在
+        if (user == null) return Result.failure(Result.ResultCode.USER_NOT_EXISTED);//用户不存在
 
         if (category == 1) {//重设密码
             redisUtil.set("RESET" + userId, code, 60 * 5);//五分钟过期
             userService.sendMail(userService.getUser(userId).getMail(), code);//发送验证码
         }
-        functionService.saveRecord(BehavioralRecordBo.RecordCode.GET_CODE.getMessage(), code);
+        // todo 取消鉴权逻辑
+//        functionService.saveRecord(BehavioralRecordBo.RecordCode.GET_CODE.getMessage(), code);
         return Result.success(code);//验证码返回
 
 
     }
 
     @PostMapping("/reset/password")//重置密码
-    public Result retrievePassword(@RequestParam String userId,
-                                   @RequestParam String code, @RequestParam String newPassword) {//验证码
+    public Result retrievePassword(@RequestParam String userId, @RequestParam String code, @RequestParam String newPassword) {//验证码
 
         String userCode = (String) redisUtil.get("RESET" + userId);//用户验证码
         if (userCode == null)//验证码不存在
@@ -133,8 +129,8 @@ public class UserController {
 
             User user = userService.resetPassword(userId, passwordEncoder.encode(newPassword));//重设密码
 
-
-            functionService.saveRecord(BehavioralRecordBo.RecordCode.MODIFY_PASSWORD.getMessage(), user.toString());
+// todo 取消鉴权逻辑
+//            functionService.saveRecord(BehavioralRecordBo.RecordCode.MODIFY_PASSWORD.getMessage(), user.toString());
 
             return Result.success("修改成功");
         } else {
